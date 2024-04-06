901a2fe8dcc0d2b7689f18839daca2c2d12b3bc0
Juan Fernando Giraldo Cardona <juanferrgiraldo@gmail.com>
2024-04-06 15:10:26 -0500

    feat: accessibility enhancement

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,16 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toastList, setToastList] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleScapeKey(event) {
+      if (event.code === 'Escape') {
+        setToastList([]);
+      }
+    }
+    window.addEventListener('keydown', handleScapeKey);
+    return () => window.removeEventListener('keydown', handleScapeKey);
+  }, []);
+
   function dismissToast(id) {
     const newToastList = toastList.filter((toast) => toast.id !== id);
     setToastList(newToastList);
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -7,7 +7,12 @@ import { ToastContext } from '../ToastProvider/ToastProvider';
 function ToastShelf() {
   const { toastList } = React.useContext(ToastContext);
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role='region'
+      aria-live='assertive'
+      aria-label='Notification'
+    >
       {toastList.map(({ id, message, variant }) => {
         return (
           <li key={id} className={styles.toastWrapper}>
